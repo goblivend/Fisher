59832bd9f479f511042a3324b8e6f57033558c51
Ivan Imbert <ivan.imbert@epita.fr>
2023-10-13 14:17:17 +0200

    fix(miner): block values

--- miner.py
+++ miner.py
@@ -5,20 +5,30 @@ import PIL
 from PIL import Image
 
 
-MAIN_REGION=(500, 500, 1500, 1000)
+MAIN_REGION=(200, 100, 1500, 800)
 
+def distPixels(p1, p2) :
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]) + abs(p1[2]-p2[2])
+
+def nearColor(c1, c2, dist = 30) :
+    return distPixels(c1, c2) < dist
 
 def checkValidity(args) :
+    print('Checking validity')
     pixels = {
-        'ores': [(139, 139, 139)],
-        'obsi': [],
-        'neth': []
+        'ores': [(71, 71, 71), (57, 57, 57)],
+        'obsi': [(29, 19, 42), (0, 0, 1)],
+        'neth': [(63, 31, 31), (32, 11, 11)],
+        'ends': [(100, 97, 67), (114, 118, 86)]
     }
     sc = pyautogui.screenshot(region=MAIN_REGION)
+    sc.save('screen.png')
     for pix in pixels[args.blocks] :
         inSc = False
         for pixel in sc.getdata():
-            if pixel == pix :
+            # print(pix, pixel, distPixels(pixel, pix))
+            if nearColor(pixel, pix) :
+                print('Found a block', pix, pixel)
                 inSc = True
                 break
         if not inSc :
@@ -36,7 +46,7 @@ def takeBreaks(args) :
     chances = args.break_chances
     times = args.break_times
     for i in range(len(chances)) :
-        if chances[i] == 0 : 
+        if chances[i] == 0 :
             continue
         if rd.randint(0, chances[i]) == 0 :
             takeABreak(rd.uniform(times[2*i], times[2*i+1]))
@@ -64,8 +74,8 @@ def mine(args):
             if not checkValidity(args) :
                 print('No blocks to mine')
                 raise KeyboardInterrupt
-            
-            move(args.left,rd.uniform(0.5, 0.8))
+
+            # move(args.left,rd.uniform(0.5, 0.8))
             time.sleep(rd.uniform(0.0, 0.2))
 
             takeBreaks(args)
@@ -73,7 +83,7 @@ def mine(args):
                 print('No blocks to mine')
                 raise KeyboardInterrupt
 
-            move(args.right,rd.uniform(0.5, 0.8))
+            # move(args.right,rd.uniform(0.5, 0.8))
 
     except KeyboardInterrupt:
         pyautogui.keyUp(args.left)
@@ -129,9 +139,9 @@ def parser(sub) :
         help='Time to mine in seconds (-1 for infinite)')
     miner_parser.add_argument('--break-chances', '-B', default=[1], type=int, metavar='n', nargs='*',
             help='Chances to take a break while mining: matching --breack-time times (0 for none, must be half of --break-times length)')
-    miner_parser.add_argument('--break-times', '-T', default=[0.1, 0.4], type=float, metavar='n', nargs='*', 
+    miner_parser.add_argument('--break-times', '-T', default=[0.1, 0.4], type=float, metavar='n', nargs='*',
         help='Bounds of breaks, must be twice as big as --break-chances (1 min and 1 max per chance)')
-    miner_parser.add_argument('--blocks', '-b', default='ores', metavar='type', choices=['ores', 'obsi', 'neth'],
+    miner_parser.add_argument('--blocks', '-b', default='ores', metavar='type', choices=['ores', 'obsi', 'neth', 'ends'],
         help='Blocks to be looking for in field of vision')
 
     miner_parser.set_defaults(fun=launch_mine)
